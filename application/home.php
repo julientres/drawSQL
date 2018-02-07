7ea88ea94f8748f640d8329b33968e4304465869
Julien <julien.trescartes@gmail.com>
2018-02-07 22:55:44 +0100

    add javascript check on form

--- application/home.php
+++ application/home.php
@@ -9,10 +9,6 @@
 		$_SESSION['passwd']=$m;		
 	}
 
-	function bla(){
-		print_r($_SESSION);
-	}
-
 	function getValueFromPost($string){
 		if(!isset($_POST['btnReinit'])){
 			if($string === 'host' && isset($_POST['host']))
@@ -52,24 +48,24 @@
 								header("Refresh: 2; URL=draw-algebraic-queries.php");
 							}
 							else{
-								$error['connexion'] = 'Impossible de se connecter à la base de données. '.$returnBDD['object']->getMessage();
-								$info['info'] = 'Vérifiez que les champs du formulaire soient corrects puis essayez à nouveau';
+								$error = 'Impossible de se connecter à la base de données. '.$returnBDD['object']->getMessage();
+								$info = 'Vérifiez que les champs du formulaire soient corrects puis essayez à nouveau';
 							}													
 						}
 						else{
-							$error['user'] = 'Veuillez saisir un identifiant';
+							//$error['user'] = 'Veuillez saisir un identifiant';
 						}					
 					}
 					else{
-						$error['bdd'] = 'Veuillez saisir une base de données';
+						//$error['bdd'] = 'Veuillez saisir une base de données';
 					}
 				}
 				else{
-					$error['port'] = 'Veuillez saisir un port';
+					//$error['port'] = 'Veuillez saisir un port';
 				}
 			}
 			else{
-				$error['host'] = 'Veuillez saisir un host';
+				//$error['host'] = 'Veuillez saisir un host';
 			}
 		}
 	}
@@ -122,63 +118,68 @@
 			border: 0;
 			text-align:center;
 		}
+		#inputHostError, #inputPortError, #inputBDDError, #inputUserError{
+			margin-bottom:0;
+			color: #F22613;
+		}
 	</style>
 </head>
 <body>
 	<div id="connexion_bloc">
 		<?php
 			if(isset($error)){
-				foreach($error as $e){
-					echo "<div class='alert alert-danger alert-dismissible' role='alert'>
+				echo "<div class='alert alert-danger alert-dismissible' role='alert'>
 						<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
-						<span class='glyphicon glyphicon-ban-circle' aria-hidden='true'></span><strong> Erreur !</strong> ".$e."
-						</div>";
-				}
+						<span class='glyphicon glyphicon-ban-circle' aria-hidden='true'></span><strong> Erreur !</strong> ".$error."
+					</div>";
 			}
 
-			if(isset($info)){
-				foreach($info as $i){				
-					echo "<div class='alert alert-info alert-dismissible' role='alert'>
+			if(isset($info)){				
+				echo "<div class='alert alert-info alert-dismissible' role='alert'>
 						<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
-							<span class='glyphicon glyphicon-info-sign' aria-hidden='true'></span><strong> Information :</strong> ".$i."
-						</div>";	
-				}			
+							<span class='glyphicon glyphicon-info-sign' aria-hidden='true'></span><strong> Information :</strong> ".$info."
+					</div>";			
 			}
 
 			if(isset($success)){
 				echo "<div class='alert alert-success alert-dismissible' role='alert'>
 						<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
-						<span class='glyphicon glyphicon-ban-circle' aria-hidden='true'></span><strong> Succès !</strong> ".$success."
+						<span class='glyphicon glyphicon-ok-circle' aria-hidden='true'></span><strong> Succès !</strong> ".$success."
 					</div>";
 			}
 		?>
 
-		<form method="POST" action="home.php" id="connexion_form">
+		<form method="POST" action="home.php" id="connexion_form" onsubmit="return checkForm(this);">
+			<div id="errorForm"></div>
 			<h3>Application de requêtes algébriques - Connexion</h3>
 			<hr>
 			<br><br>
 			<div class="form-group row">
 			    <label for="host" class="col-lg-4 col-form-label">Host*</label>
 			    <div class="col-lg-6">
-			      <input type="text" class="form-control" id="host" name="host" value="<?= getValueFromPost('host') ?>" placeholder="ex : localhost">
+			      <input type="text" class="form-control" id="host" name="host" value="<?= getValueFromPost('host') ?>" placeholder="ex : localhost" onblur="checkHost(this);">
+			      <div id="inputHostError"></div>
 				</div>
 			</div>
 			<div class="form-group row">
 			    <label for="port" class="col-lg-4 col-form-label">Port*</label>
 			    <div class="col-lg-6">
-			      <input type="text" class="form-control" id="port" name="port" value="<?= getValueFromPost('port') ?>" placeholder="ex : 3306" maxlength="4">
+			      <input type="text" class="form-control" id="port" name="port" value="<?= getValueFromPost('port') ?>" placeholder="ex : 3306" maxlength="4" onblur="checkPort(this);">
+			      <div id="inputPortError"></div>
 				</div>
 			</div>
 			<div class="form-group row">
 			    <label for="bdd" class="col-lg-4 col-form-label">BDD*</label>
 			    <div class="col-lg-6">
-			      <input type="text" class="form-control" id="bdd" name="bdd" value="<?= getValueFromPost('bdd') ?>" placeholder="ex : nombdd">
+			      <input type="text" class="form-control" id="bdd" name="bdd" value="<?= getValueFromPost('bdd') ?>" placeholder="ex : nombdd" onblur="checkBDD(this);">
+			      <div id="inputBDDError"></div>
 				</div>
 			</div>
 			<div class="form-group row">
 			    <label for="user" class="col-lg-4 col-form-label">Identifiant*</label>
 			    <div class="col-lg-6">
-			      <input type="text" class="form-control" id="user" name="user" value="<?= getValueFromPost('user') ?>" placeholder="ex : user">
+			      <input type="text" class="form-control" id="user" name="user" value="<?= getValueFromPost('user') ?>" placeholder="ex : user" onblur="checkUser(this);">
+			      <div id="inputUserError"></div>
 				</div>
 			</div>
 			<div class="form-group row">
@@ -191,10 +192,120 @@
 			<br>
 			<p><i>* Champs obligatoires</i></p>
 			<div class="btn-group" role="group" aria-label="...">			
-				<button type="submit" name="btnCo" class="btn btn-primary active">Se connecter <span class="glyphicon glyphicon-log-in" aria-hidden="true"></span></button>
+				<button type="submit" name="btnCo" id="btnCo" class="btn btn-primary active">Se connecter <span class="glyphicon glyphicon-log-in" aria-hidden="true"></span></button>
 				<button type="reset" name="btnReinit" class="btn btn-warning active">Réinitialiser <span class="glyphicon glyphicon-refresh" aria-hidden="true"></span></button>
 			</div>
 		</form>
 	</div>
+
+	<script type="text/javascript">
+		function loadError(champ, erreur) {
+			if(erreur) {
+				champ.style.borderColor = "#F22613";
+				champ.style.borderWidth = "1px";
+				var div = champ.name;
+				switch(div) {
+					case 'host':
+						document.getElementById("inputHostError").innerHTML = "Le champ [Host] est obligatoire";
+						break;
+					case 'port':
+						document.getElementById("inputPortError").innerHTML = "Le champ [Port] est obligatoire";
+						break;
+					case 'bdd':
+						document.getElementById("inputBDDError").innerHTML = "Le champ [BDD] est obligatoire";
+						break;
+					case 'user':
+						document.getElementById("inputUserError").innerHTML = "Le champ [Identifiant] est obligatoire";
+						break;
+				}
+			}	
+			else {
+				champ.style.borderColor = "#26A65B";
+				champ.style.borderWidth = "1px";
+				var div = champ.name;
+				switch(div) {
+					case 'host':
+						document.getElementById("inputHostError").innerHTML = "";
+						break;
+					case 'port':
+						document.getElementById("inputPortError").innerHTML = "";
+						break;
+					case 'bdd':
+						document.getElementById("inputBDDError").innerHTML = "";
+						break;
+					case 'user':
+						document.getElementById("inputUserError").innerHTML = "";
+						break;
+				}
+			}		
+		}
+
+		function checkForm(form) {
+			var hostOK = checkHost(form.host);
+			var portOK = checkPort(form.port);
+		    var bddOK = checkBDD(form.bdd);
+		    var userOK = checkUser(form.user);
+		   
+		    if(hostOK && portOK && bddOK && userOK)
+		       return true;
+		    else
+		    {
+		    	document.getElementById("errorForm").innerHTML = "<div class='alert alert-danger alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><span class='glyphicon glyphicon-ban-sign' aria-hidden='true'></span><strong> Erreur !</strong> Veuillez remplir correctement tous les champs</div>";
+		    	return false;
+		    }
+		}
+
+		function checkHost(champ) {
+		    if(champ.value.length == 0)
+		    {
+		        loadError(champ, true);
+		    	return false;
+		    }
+		    else
+		    {
+		        loadError(champ, false);
+		        return true;
+		    }
+		}
+
+		function checkPort(champ) {
+		    if(champ.value.length < 1 || champ.value.length > 4)
+		    {
+		        loadError(champ, true);
+		        return false;
+		    }
+		    else
+ 		    {
+		        loadError(champ, false);
+		        return true;
+		    }
+		}	
+
+		function checkBDD(champ) {
+		    if(champ.value.length == 0)
+		    {
+		        loadError(champ, true);
+		        return false;
+		    }
+		    else
+		    {
+		        loadError(champ, false);
+		        return true;
+		    }
+		}
+
+		function checkUser(champ) {
+		    if(champ.value.length == 0)
+		    {
+		        loadError(champ, true);
+		        return false;
+		    }
+		    else
+		    {
+		        loadError(champ, false);
+		        return true;
+		    }
+		}	
+	</script>
 </body>
 </html>
